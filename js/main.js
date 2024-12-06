04c064716c3a3b8a7a9cca92395e473c45df5e1a
Max <101049443+Shvets-Maxx@users.noreply.github.com>
2024-12-06 22:28:29 +0200

    change

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 let nav_list = document.querySelector('.nav_list')
 let SectionBurger = document.querySelector('.section_burger')
 let burgerMenu = document.querySelectorAll('.burger_menu')
+let headerBlock = document.querySelector('.header_block')
+let logo = document.querySelector('.nav_logo')
+let burger_bg = document.querySelector('.burger_bg')
+let container = document.querySelector('.container')
 SectionBurger.addEventListener('click' ,() => {
     burgerMenu.forEach(items => {
         items.classList.toggle('active')
     })
+    headerBlock.classList.toggle('hidden')
+    logo.classList.toggle('hidden')
+    container.classList.toggle('h-100vh')
     burgerMenu[0].classList.contains('active')?nav_list.style.display = 'block':nav_list.style.display = 'none'
+    burger_bg.style.display == 'block' ? burger_bg.style.display = 'none' :burger_bg.style.display = 'block'
+    
 })
 
