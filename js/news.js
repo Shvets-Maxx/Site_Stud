41f8be35aa245243d64a8030fa6a837f135d4716
Max <101049443+Shvets-Maxx@users.noreply.github.com>
2025-01-29 16:22:59 +0200

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,6 +1,6 @@
 // const paginated_list = document.getElementById('paginated-list')
 
-// fetch("/js/news.json")
+// fetch("./news.json")
 // .then(response => response.json()
 // ).then(response =>{
 //     console.log(response)
